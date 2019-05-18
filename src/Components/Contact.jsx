e8271da28e743c5790d49bfb2ce54dd6b6a13bdd
Teigan H <teigan.hockman@gmail.com>
2019-05-18 15:46:13 -0400

    Update About.jsx and Contact.jsx

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -3,9 +3,17 @@ import React, { Component } from 'react'
 export default class Contact extends Component {
   render() {
     return (
-      <div>
+      <div className="contact">
         <h1>Contact</h1>
-        convenience store corporation alcohol claymore mine bridge assassin grenade face forwards grenade garage cardboard table corporation claymore mine receding. dome systema narrative footage knife pistol receding dome engine into systemic San Francisco uplink tattoo spook. order-flow sub-orbital kanji sprawl vinyl film crypto- vinyl carbon systemic carbon tiger-team wonton soup BASE jump smart-. human beef noodles realism grenade ablative kanji euro-pop pre- digital bicycle pen pistol saturation point Legba geodesic. 
+        You can find me on the interwebs! 
+        <ul>
+          <li><a href="https://www.linkedin.com/in/teiganhockman/" target="_blank">LinkedIn</a></li>
+          <li><a href="https://github.com/teiganh" target="_blank">Github</a></li>
+          <li><a href="https://www.instagram.com/culture_crime/" target="_blank">Instagram</a></li>
+          <li><a href="mailto:teigan.hockman@gmail.com?Subject=Web%20Development%20Consultation" target="_top">Email</a></li>
+        </ul>
+
+        I am also available for branding, artistic projects, and hand-drawn illustrations.
       </div>
     )
   }
